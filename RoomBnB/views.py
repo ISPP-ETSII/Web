48847373accc40a3077acd14621eb90660ac13e9
Manuel Serrano <mansergue@alum.us.es>
2018-03-20 17:44:50 +0100

    Template corregido

--- RoomBnB/views.py
+++ RoomBnB/views.py
@@ -87,14 +87,3 @@ def root(request):
 
 def base(request):
     return render(request, template_name='index.html')
-
-
-def upload_pic(request):
-    if request.method == 'POST':
-        form = ImageUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            m = ExampleModel.objects.get(pk=course_id)
-            m.model_pic = form.cleaned_data['image']
-            m.save()
-            return HttpResponse('image upload success')
-    return HttpResponseForbidden('allowed only via POST')
