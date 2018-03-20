22f226915ac1e47230c36e6a21dad5c99bff1727
gonlopher <gonlopher@alum.us.es>
2018-03-20 22:46:00 +0100

    archivos nuevos subidos

---
+++ RoomBnB/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from RoomBnB.models import Room
+from RoomBnB.models import User
+from RoomBnB.models import Flat
+from django.db import models
+
+
+class FlatForm(forms.Form):
+    address = forms.CharField(label='Address', max_length=100)
+    description = forms.CharField(label='Description', max_length=500)
+
+class ReviewForm(forms.Form):
+    RATINGS = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+    )
+
+    title = forms.CharField(label='title', max_length=50)
+    description = forms.CharField(label='description', max_length=500)
+    date = forms.DateField(label='date')
+    rating = forms.CharField(label='rating', max_length=1)
+
+class UserReviewForm(ReviewForm):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class FlatReviewForm(ReviewForm):
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE)
+
+class RoomReviewForm(ReviewForm):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+
+
+    #class Meta:
+     #   models = Flat
+     #  fields = ['description','pictures','owner']
