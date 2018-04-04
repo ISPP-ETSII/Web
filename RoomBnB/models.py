4628cfee694dd80b5412ea38815278c2a2bccf67
gonlopher <gonlopher@alum.us.es>
2018-04-04 18:13:44 +0200

    Subida de archivos, falla el owner

--- RoomBnB/models.py
+++ RoomBnB/models.py
@@ -4,31 +4,79 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-class CreditCard(models.Model):
-    owner = models.CharField(max_length=50)
-    code = models.CharField(max_length=16)
-    cvv = models.CharField(max_length=3)
-
-
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField()
-    credit_card = models.OneToOneField(CreditCard, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(upload_to='profile/', default='profile/generic/default.png')
+
+
+class UserProperties(models.Model):
+    RATINGS = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+    )
+
+    DEGREES = (
+        ('1', 'Enfermeria'),
+        ('2', 'Ing. Informatica'),
+        ('3', 'Medicina'),
+        ('4', 'Biologia'),
+        ('5', 'Arquitectura'),
+    )
+
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    smoker = models.BooleanField(default=False)
+    pets = models.BooleanField(default=False)
+    sporty = models.CharField(max_length=1, choices=RATINGS)
+    gamer = models.CharField(max_length=1, choices=RATINGS)
+    sociable = models.CharField(max_length=1, choices=RATINGS)
+    degree = models.CharField(max_length=1, choices=DEGREES)
 
 
 class Flat(models.Model):
+    title = models.TextField(max_length=100)
+    address = models.TextField(max_length=100)
     description = models.TextField(max_length=500)
-    pictures = models.ImageField()
-    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='flat/', default='flat/generic/default.jpg')
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='flats')
+
+
+class FlatProperties(models.Model):
+    flat = models.OneToOneField(Flat, on_delete=models.CASCADE)
+    elevator = models.BooleanField(default=False)
+    washdisher = models.BooleanField(default=False)
 
 
 class Room(models.Model):
     description = models.TextField(max_length=500)
     price = models.FloatField()
-    pictures = models.ImageField()
-    temporal_owner = models.ForeignKey(Profile, on_delete=models.DO_NOTHING)
-    belong_to = models.ForeignKey(Flat, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='room/', default='room/generic/default.jpg')
+    temporal_owner = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True)
+    belong_to = models.ForeignKey(Flat, on_delete=models.CASCADE, related_name='rooms')
+
+
+class RoomProperties(models.Model):
+    BEDS = (
+        ('1', 'Couple'),
+        ('2', 'Single'),
+        ('3', 'Sofa'),
+        ('4', 'None'),
+    )
+
+    room = models.OneToOneField(Room, on_delete=models.CASCADE)
+    balcony = models.BooleanField(default=False)
+    window = models.BooleanField(default=False)
+    air_conditioner = models.BooleanField(default=False)
+    bed = models.CharField(max_length=1, choices=BEDS, default=2)
+
+
+class RentRequest(models.Model):
+    requester = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rent_requests')
+    creation_date = models.DateField(auto_now_add=True)
+    requested = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='rent_requests')
+    accepted = models.BooleanField(default=False)
 
 
 class Contract(models.Model):
@@ -42,7 +90,6 @@ class Contract(models.Model):
 class Payment(models.Model):
     amount = models.FloatField()
     date = models.DateField(auto_now_add=True)
-    credit_card = models.ForeignKey(CreditCard, on_delete=models.DO_NOTHING)
     contract = models.ForeignKey(Contract, on_delete=models.CASCADE, default=None)
 
 
@@ -72,3 +119,14 @@ class FlatReview(Review):
 class RoomReview(Review):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
+
+class Image(models.Model):
+    image = models.ImageField()
+
+
+class FlatImage(Image):
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE)
+
+
+class RoomImage(Image):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
