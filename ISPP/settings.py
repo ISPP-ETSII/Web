dfa766b86f122ca249082925db5d4c529e0177ca
Alejandro Hall <alejandro@geographica.gs>
2018-04-06 12:57:41 +0200

    Revert "Update travis file"
    This reverts commit 0f93e4825a7461557785782e47533d4ed0234449.

--- ISPP/settings.py
+++ ISPP/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,8 +90,6 @@ DATABASES = {
     }
 }
 
-if 'test' in sys.argv or 'test_coverage' in sys.argv:
-    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
