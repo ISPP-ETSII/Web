e9cef5ae3e5db5dff0682b5e72623d985d61ce3e
caolin333 <raulrp1996@gmail.com>
2018-04-10 20:38:16 +0200

    Test del Filtrado de Casas Ref#46

--- RoomBnB/tests.py
+++ RoomBnB/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.test import TestCase
+from django.core.files import File
+from RoomBnB.services import *
+from RoomBnB.views import *
 
 # Create your tests here.
+
+class TestHouseFilter(TestCase):
+
+    #Test de filtrado
+
+    def testFilteredHouse(self, keyword,elevator,washdisher,balcony,window,air_conditioner):
+
+        flats = get_flats_filtered(keyword, elevator, washdisher, balcony, window, air_conditioner)
+
+        for flat in flats:
+
+            #check keyword
+            if keyword not in flat.title: print("Error in title")
+            if keyword not in flat.description: print("Error in description")
+            if keyword not in flat.address: print("Error in address")
+
+            #check flat properties
+            flatProps = FlatProperties.objects.get(flat=flat)
+            if flatProps.elevator != elevator: print("Error in elevator")
+            if flatProps.washdisher != washdisher: print("Error in washdisher")
+
+            #check room properties
+            roomList = Room.objects.filter(belong_to=flat)
+            for room in roomList:
+                roomProps = RoomProperties.objects.get(room=room)
+                if roomProps.balcony != balcony: print("Error in balcony")
+                if roomProps.window != balcony: print("Error in window")
+                if roomProps.air_conditioner != balcony: print("Error in air conditioner")
