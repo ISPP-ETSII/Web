b90094d09bfb225f727e308245b0e3374d5bfc30
caolin333 <raulrp1996@gmail.com>
2018-04-11 12:56:35 +0200

    Correccion de los tests Ref#46

--- RoomBnB/tests.py
+++ RoomBnB/tests.py
@@ -10,26 +10,31 @@ class TestHouseFilter(TestCase):
 
     #Test de filtrado
 
-    def testFilteredHouse(self, keyword,elevator,washdisher,balcony,window,air_conditioner):
+    def testFilteredHouse(self):
 
-        flats = get_flats_filtered(keyword, elevator, washdisher, balcony, window, air_conditioner)
+        #user creation for the test
+        userTest = User.objects.create_user(username="userTest", email="user@prueba.com")
+        userTest.set_password("userTest")
+        userTest.save()
+
+        #flat creation for the test
+        title, address, description1 = "PisoTest", "Bami", "Piso muy guay"
+        create_flat(title, address, description1, '', userTest)
+        flatSaved = Flat.objects.get(title=title)
+        flatSavedProps = FlatProperties.objects.create(flat=flatSaved, elevator=True, washdisher=True)
+
+        #room creation for the test
+        description2, price, belong_to = "buena habitacion", 150, flatSaved
+        roomSaved = Room.objects.create(description=description2, price=price, picture='', temporal_owner='', belong_to=belong_to)
+        roomSavedProps = RoomProperties.objects.create(room=roomSaved, balcony=True, window=True, air_conditioner=True)
+
+        #get flats with properties
+        flats = get_flats_filtered("PisoTest", True, True, True, True, True)
 
         for flat in flats:
 
-            #check keyword
-            if keyword not in flat.title: print("Error in title")
-            if keyword not in flat.description: print("Error in description")
-            if keyword not in flat.address: print("Error in address")
-
-            #check flat properties
-            flatProps = FlatProperties.objects.get(flat=flat)
-            if flatProps.elevator != elevator: print("Error in elevator")
-            if flatProps.washdisher != washdisher: print("Error in washdisher")
-
-            #check room properties
-            roomList = Room.objects.filter(belong_to=flat)
-            for room in roomList:
-                roomProps = RoomProperties.objects.get(room=room)
-                if roomProps.balcony != balcony: print("Error in balcony")
-                if roomProps.window != balcony: print("Error in window")
-                if roomProps.air_conditioner != balcony: print("Error in air conditioner")
+            self.assertEqual(flat, flatSaved)
+            self.assertEqual(FlatProperties.objects.get(flat=flat), flatSavedProps)
+            roomToCheck = Room.objects.filter(belong_to=flat)
+            self.assertEqual(roomToCheck, roomSaved)
+            self.assertEqual(RoomProperties.objects.get(room=roomToCheck), roomSavedProps)
